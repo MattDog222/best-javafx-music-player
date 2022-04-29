4b5dda10da2fa88f2ae4f604b98a6b6c75a86ba9
MattDog222 <104606640+MattDog222@users.noreply.github.com>
2022-04-28 22:00:11 -0500

    Updated title

--- src/main/java/suic/LibrarySelectionView.java
+++ src/main/java/suic/LibrarySelectionView.java
@@ -29,7 +29,7 @@ public class LibrarySelectionView extends AnchorPane {
             Scene scene = new Scene(loader.load());
 
             Stage stage = new Stage();
-            stage.setTitle("Best Library Selector");
+            stage.setTitle("Select Libraries");
             stage.setScene(scene);
             stage.show();
 
